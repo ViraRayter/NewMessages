c757df0bd73c751cb75a9c1feae25fca5d966656
sashan203 <nagovicyn203@mail.ru>
2022-03-21 12:06:19 +0300

    add index.html

--- backup/umain.pas
+++ backup/umain.pas
@@ -5,7 +5,7 @@ unit umain;
 interface
 
 uses
-  Classes, SysUtils, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
 
 type
 
